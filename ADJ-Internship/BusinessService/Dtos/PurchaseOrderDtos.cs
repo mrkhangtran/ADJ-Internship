566edee10dbd1990e96ed50faffcf2d7d0e56669
Tôn Thất Duy Khang <duykhang91@gmail.com>
2018-12-12 10:02:22 +0700

    Adding DTOs and Validators

--- ADJ-Internship/BusinessService/Dtos/PurchaseOrderDtos.cs
+++ ADJ-Internship/BusinessService/Dtos/PurchaseOrderDtos.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using ADJ.BusinessService.Core;
 using ADJ.DataModel;
 using ADJ.DataModel.Core;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ADJ.BusinessService.Dtos
 {
@@ -27,5 +29,90 @@ namespace ADJ.BusinessService.Dtos
         {
             profile.CreateMap<CreateOrUpdatePurchaseOrderRq, PurchaseOrder>().IncludeBase<EntityDtoBase, EntityBase>();
         }
+    }
+
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "PO Number")]
+        [StringLength(10, ErrorMessage = "Cannot be longer than 10 characters")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Letters and numbers only")]
+        public string PONumber { get; set; }
+
+        //DropList from 2010 to 2020
+        //Default value is Current Date
+        [Display(Name = "Order Date")]
+        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        [StringLength(30, ErrorMessage = "Cannot be longer than 30 character")]
+        public string Buyer { get; set; }
+
+        //Default value = USD
+        public string Currency { get; set; } = "USD";
+
+        //Droplist from 2010 to 2020
+        public string Season { get; set; }
+        public IEnumerable<SelectListItem> Seasons { get; set; }
+
+        [StringLength(30, ErrorMessage = "Cannot be longer than 30 characters")]
+        public string Department { get; set; }
+
+        [StringLength(30, ErrorMessage = "Cannot be longer than 30 characters")]
+        public string Vendor { get; set; }
+
+        [StringLength(30, ErrorMessage = "Cannot be longer than 30 characters")]
+        public string Company { get; set; }
+
+        //Droplist Vietnam-HongKong
+        [Required]
+        public string Origin { get; set; }
+        public IEnumerable<SelectListItem> Origins { get; set; }
+
+        //Droplist of Ports
+        [Required]
+        [Display(Name = "Port of Loading")]
+        [PortIsDifferent("PortOfDelivery")]
+        public string PortOfLoading { get; set; }
+
+        [Required]
+        [Display(Name = "Port of Delivery")]
+        [PortIsDifferent("PortOfLoading")]
+        public string PortOfDelivery { get; set; }
+        public IEnumerable<SelectListItem> Ports { get; set; }
+
+        [Display(Name = "Order Type")]
+        [StringLength(30, ErrorMessage = "Cannot be longer than 30 characters")]
+        public string OrderType { get; set; }
+
+        [StringLength(30, ErrorMessage = "Cannot be longer than 30 characters")]
+        public string Factory { get; set; }
+
+        //DropList Road-Sea-Air
+        [Display(Name = "Ship Method")]
+        public string Mode { get; set; }
+        public IEnumerable<SelectListItem> Modes { get; set; }
+
+        [Display(Name = "Ship Date")]
+        public DateTime ShipDate { get; set; }
+
+        [Display(Name = "Latest Ship Date")]
+        [SimilarOrLaterThanShipDate("ShipDate")]
+        public DateTime LatestShipDate { get; set; }
+
+        [Display(Name = "Delivery Date")]
+        [SimilarOrLaterThanShipDate("ShipDate")]
+        public DateTime DeliveryDate { get; set; }
+
+        //sum of all PODetails Quantity 
+        public float POQuantity { get; set; }
+
+        //Default value = "New"
+        public string Status { get; set; } = "New";
+
+        public OrderDetailDTO orderDetailDTO { get; set; }
+
+        public virtual List<OrderDetailDTO> PODetails { get; set; }
     }
 }
--- ADJ-Internship/BusinessService/Validators/PurchaseOrderDtoValidators.cs
+++ ADJ-Internship/BusinessService/Validators/PurchaseOrderDtoValidators.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using ADJ.BusinessService.Dtos;
 using FluentValidation;
@@ -11,6 +12,58 @@ namespace ADJ.BusinessService.Validators
         public CreateOrUpdatePurchaseOrderRqValidator()
         {
             RuleFor(x => x.Test).NotEmpty();
+        }
+    }
+
+    public class PortIsDifferent : ValidationAttribute
+    {
+        private readonly string _otherProperty;
+
+        public PortIsDifferent(string otherProperty)
+        {
+            _otherProperty = otherProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, System.ComponentModel.DataAnnotations.ValidationContext validationContext)
+        {
+            var otherValue = validationContext.ObjectType.GetProperty(_otherProperty).GetValue(validationContext.ObjectInstance, null);
+
+            if ((value != null) && (otherValue != null))
+            {
+                if (value.ToString().CompareTo(otherValue.ToString()) == 0)
+                {
+                    return new ValidationResult(ErrorMessage = "Ports cannot be the same");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+    public class SimilarOrLaterThanShipDateAttribute : ValidationAttribute
+    {
+        private readonly string _otherProperty;
+
+        public SimilarOrLaterThanShipDateAttribute(string otherProperty)
+        {
+            _otherProperty = otherProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, System.ComponentModel.DataAnnotations.ValidationContext validationContext)
+        {
+            var otherValue = validationContext.ObjectType.GetProperty(_otherProperty).GetValue(validationContext.ObjectInstance, null);
+
+            if ((value != null) && (otherValue != null))
+            {
+                DateTime date1 = Convert.ToDateTime(value);
+                DateTime date2 = Convert.ToDateTime(otherValue);
+                if (date1 < date2)
+                {
+                    return new ValidationResult(ErrorMessage = "Cannot be before Ship Date");
+                }
+            }
+
+            return ValidationResult.Success;
         }
     }
 }
