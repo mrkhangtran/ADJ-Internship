bd8098c936aa0c8e08ac8460ef6e9c52db8c934f
tannguyen2602 <tan.nguyen@groovetechnology.com>
2018-12-12 10:59:33 +0700

    fix

--- ADJ-Internship/DataModel/ShipmentTrack/ArriveOfDespacth.cs
+++ ADJ-Internship/DataModel/ShipmentTrack/ArriveOfDespacth.cs
@@ -7,18 +7,24 @@ namespace ADJ.DataModel.ShipmentTrack
 {
     public class ArriveOfDespacth : EntityBase
     {
-        //[ForeignKey("BookingModel")]
         public int BookingId { get; set; }
+
         public string Carrier { get; set; }
+
         public string Vessel { get; set; }
+
         public string Voyage { get; set; }
-        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+
         public DateTime CTD { get; set; }
-        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+
         public DateTime ETA { get; set; }
+
         public string OriginPort { get; set; }
+
         public string DestinationPort { get; set; }
+
         public string Mode { get; set; }
+
         public string Confirmed { get; set; }
 
         public virtual Booking Booking { get; set; }
--- ADJ-Internship/DataModel/ShipmentTrack/Booking.cs
+++ ADJ-Internship/DataModel/ShipmentTrack/Booking.cs
@@ -8,15 +8,21 @@ namespace ADJ.DataModel.ShipmentTrack
     public class Booking : EntityBase
     {
         public string Order { get; set; }
+
         public int OrderId { get; set; }
+
         public string Line { get; set; }
+
         public string Item { get; set; }
+
         public string Carier { get; set; }
+
         public string Vessel { get; set; }
-        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+
         public DateTime ETD { get; set; }
-        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+
         public DateTime ETA { get; set; }
+
         public string Voyage { get; set; }
 
         public int Quantity { get; set; }
@@ -24,14 +30,21 @@ namespace ADJ.DataModel.ShipmentTrack
         public int Cartoons { get; set; }
 
         public decimal Cube { get; set; }
-        public string PackType { get; set; }
+
+        public string PackType { get; set;}
+
         public string LoadingType { get; set; }
+
         public string Mode { get; set; }
+
         public string FreightTerms { get; set; }
+
         public string Consignee { get; set; }
 
         public decimal GrossWeight { get; set; }
+
         public DateTime BookingDate { get; set; }
+
         public string BookingType { get; set; }
     }
 }
