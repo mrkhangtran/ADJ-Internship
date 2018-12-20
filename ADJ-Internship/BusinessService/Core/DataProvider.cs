46bf256709745be6cf791f5377f4637a1951760c
tannguyen2602 <tan.nguyen@groovetechnology.com>
2018-12-20 10:43:14 +0700

    working

--- ADJ-Internship/BusinessService/Core/DataProvider.cs
+++ ADJ-Internship/BusinessService/Core/DataProvider.cs
@@ -12,127 +12,129 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ADJ.BusinessService.Core
 {
-    public class DataProvider<T> : IDataProvider<T> where T : EntityBase
-    {
-        private readonly IRepository<T> _repo;
+	public class DataProvider<T> : IDataProvider<T> where T : EntityBase
+	{
+		private readonly IRepository<T> _repo;
 
-        public DataProvider(IRepository<T> repo)
-        {
-            _repo = repo;
-        }
+		public DataProvider(IRepository<T> repo)
+		{
+			_repo = repo;
+		}
 
-        public async Task<T> GetByIdAsync(int id)
-        {
-            var entity = await _repo.GetByIdAsync(id, true);
-            return entity;
-        }
 
-        public async Task<PagedListResult<T>> ListAsync(Expression<Func<T, bool>> filter = null, string sortData = null, bool includeDependents = false, int? pageIndex = null, int? pageSize = null)
-        {
-            List<T> entities;
 
-            // filtering
-            var query = filter != null ? _repo.Query(filter, includeDependents) : _repo.Query(includeDependents);
+		public async Task<T> GetByIdAsync(int id)
+		{
+			var entity = await _repo.GetByIdAsync(id, true);
+			return entity;
+		}
 
-            // order by
-            if (sortData != null)
-            {
-                //var orderBy = TransformOrderByClause(sortData);
-                query = query.OrderBy(sortData);
-            }
+		public async Task<PagedListResult<T>> ListAsync(Expression<Func<T, bool>> filter = null, string sortData = null, bool includeDependents = false, int? pageIndex = null, int? pageSize = null)
+		{
+			List<T> entities;
 
-            // paging
-            int totalCount;
-            int pageCount = 1;
-            if (pageIndex != null && pageSize != null)
-            {
-                entities = await query.SelectPagingAsync(pageIndex.Value, pageSize.Value, out totalCount, out pageCount);
-            }
-            else
-            {
-                entities = await query.SelectAsync();
-                totalCount = entities.Count();
-            }
+			// filtering
+			var query = filter != null ? _repo.Query(filter, includeDependents) : _repo.Query(includeDependents);
 
-            // result
-            var result = new PagedListResult<T>
-            {
-                TotalCount = totalCount,
-                PageCount = pageCount,
-                Items = entities
-            };
+			// order by
+			if (sortData != null)
+			{
+				//var orderBy = TransformOrderByClause(sortData);
+				query = query.OrderBy(sortData);
+			}
 
-            return result;
-        }
+			// paging
+			int totalCount;
+			int pageCount = 1;
+			if (pageIndex != null && pageSize != null)
+			{
+				entities = await query.SelectPagingAsync(pageIndex.Value, pageSize.Value, out totalCount, out pageCount);
+			}
+			else
+			{
+				entities = await query.SelectAsync();
+				totalCount = entities.Count();
+			}
 
-        /// <summary>
-        /// Constructs the order by clause based on the provided sort data. Sort terms must match items in
-        /// the OrderByFieldMappings dictionary.
-        /// </summary>
-        /// <param name="sortData">Sort data in the format "DTOField1,asc;DTOField2,desc;DTOField3"
-        /// Default sort order is asc</param>
-        /// <returns>string use in dynamic LinQ in format: "Field1 asc,Field2 desc,Field3 asc"</returns>
-        private string TransformOrderByClause(SortData sortData)
-        {
-            try
-            {
-                var orderByClause = new StringBuilder();
-                var hasPrimaryKeyValue = sortData.OrderByFieldMappings.ContainsKey("PrimaryKey");
-                var firstOrderByColumnSortOrder = string.Empty;
-                var containPrimaryKeyValue = false;
+			// result
+			var result = new PagedListResult<T>
+			{
+				TotalCount = totalCount,
+				PageCount = pageCount,
+				Items = entities
+			};
 
-                // If no sortby field is supplied, use the default.
-                if (string.IsNullOrEmpty(sortData.Expression))
-                {
-                    return sortData.OrderByFieldMappings["default"] + " asc";
-                }
+			return result;
+		}
 
-                var sortItems = sortData.Expression.Split(';');
+		/// <summary>
+		/// Constructs the order by clause based on the provided sort data. Sort terms must match items in
+		/// the OrderByFieldMappings dictionary.
+		/// </summary>
+		/// <param name="sortData">Sort data in the format "DTOField1,asc;DTOField2,desc;DTOField3"
+		/// Default sort order is asc</param>
+		/// <returns>string use in dynamic LinQ in format: "Field1 asc,Field2 desc,Field3 asc"</returns>
+		private string TransformOrderByClause(SortData sortData)
+		{
+			try
+			{
+				var orderByClause = new StringBuilder();
+				var hasPrimaryKeyValue = sortData.OrderByFieldMappings.ContainsKey("PrimaryKey");
+				var firstOrderByColumnSortOrder = string.Empty;
+				var containPrimaryKeyValue = false;
 
-                foreach (var sortItem in sortItems)
-                {
-                    if (!string.IsNullOrEmpty(sortItem))
-                    {
-                        var splitedSortItems = sortItem.Split(',');
-                        var orderByColumnName = splitedSortItems[0];
+				// If no sortby field is supplied, use the default.
+				if (string.IsNullOrEmpty(sortData.Expression))
+				{
+					return sortData.OrderByFieldMappings["default"] + " asc";
+				}
 
-                        if (hasPrimaryKeyValue && string.Compare(sortData.OrderByFieldMappings[orderByColumnName], sortData.OrderByFieldMappings["PrimaryKey"], StringComparison.CurrentCultureIgnoreCase) == 0)
-                        {
-                            containPrimaryKeyValue = true;
-                        }
+				var sortItems = sortData.Expression.Split(';');
 
-                        orderByClause.Append(sortData.OrderByFieldMappings[orderByColumnName]);
+				foreach (var sortItem in sortItems)
+				{
+					if (!string.IsNullOrEmpty(sortItem))
+					{
+						var splitedSortItems = sortItem.Split(',');
+						var orderByColumnName = splitedSortItems[0];
 
-                        if (splitedSortItems.Length == 2)
-                        {
-                            var sortOrder = splitedSortItems[1].Equals("desc") ? " desc," : " asc,";
-                            orderByClause.Append(sortOrder);
-                            if (string.IsNullOrEmpty(firstOrderByColumnSortOrder))
-                            {
-                                firstOrderByColumnSortOrder = sortOrder;
-                            }
-                        }
-                        else
-                        {
-                            orderByClause.Append(" asc,");  // Default sort order is asc
-                        }
-                    }
-                }
+						if (hasPrimaryKeyValue && string.Compare(sortData.OrderByFieldMappings[orderByColumnName], sortData.OrderByFieldMappings["PrimaryKey"], StringComparison.CurrentCultureIgnoreCase) == 0)
+						{
+							containPrimaryKeyValue = true;
+						}
 
-                if (hasPrimaryKeyValue && !containPrimaryKeyValue)
-                {
-                    orderByClause.Append(sortData.OrderByFieldMappings["PrimaryKey"]);
-                    orderByClause.Append(!string.IsNullOrEmpty(firstOrderByColumnSortOrder)
-                        ? firstOrderByColumnSortOrder
-                        : " asc,");
-                }
+						orderByClause.Append(sortData.OrderByFieldMappings[orderByColumnName]);
 
-                return orderByClause.ToString().TrimEnd(',');
-            }
-            catch (Exception)
-            {
-                throw new AppException("Invalid sort parameters: {0}", sortData.Expression);
-            }
-        }
-    }
+						if (splitedSortItems.Length == 2)
+						{
+							var sortOrder = splitedSortItems[1].Equals("desc") ? " desc," : " asc,";
+							orderByClause.Append(sortOrder);
+							if (string.IsNullOrEmpty(firstOrderByColumnSortOrder))
+							{
+								firstOrderByColumnSortOrder = sortOrder;
+							}
+						}
+						else
+						{
+							orderByClause.Append(" asc,");  // Default sort order is asc
+						}
+					}
+				}
+
+				if (hasPrimaryKeyValue && !containPrimaryKeyValue)
+				{
+					orderByClause.Append(sortData.OrderByFieldMappings["PrimaryKey"]);
+					orderByClause.Append(!string.IsNullOrEmpty(firstOrderByColumnSortOrder)
+							? firstOrderByColumnSortOrder
+							: " asc,");
+				}
+
+				return orderByClause.ToString().TrimEnd(',');
+			}
+			catch (Exception)
+			{
+				throw new AppException("Invalid sort parameters: {0}", sortData.Expression);
+			}
+		}
+	}
 }
--- ADJ-Internship/Common/PagedListResult.cs
+++ ADJ-Internship/Common/PagedListResult.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 
 namespace ADJ.Common
 {
-    public class PagedListResult<T>
-    {
-        public List<T> Items { get; set; }
+	public class PagedListResult<T>
+	{
+		public List<T> Items { get; set; }
 
-        public int TotalCount { get; set; }
+		public int TotalCount { get; set; }
 
-        public int PageCount { get; set; }
+		public int PageCount { get; set; }
 
-				public int? PageIndex { get; set; }
-        
-    }
+		public int? PageIndex { get; set; }
+
+		public string CurrentFilter { get; set; }
+
+	}
 }
