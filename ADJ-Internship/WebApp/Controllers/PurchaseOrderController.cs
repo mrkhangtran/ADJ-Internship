c98d3ad8b275f67a20af692cfcf46785add9b15d
Tôn Thất Duy Khang <duykhang91@gmail.com>
2018-12-27 09:58:29 +0700

    shorten code:     1. update on paging     2. remove unnecessary ViewBags change Popup after Create/Edit/Copy order on page, then redirect, instead of redirect and show popup

--- ADJ-Internship/WebApp/Controllers/PurchaseOrderController.cs
+++ ADJ-Internship/WebApp/Controllers/PurchaseOrderController.cs
@@ -57,7 +57,6 @@ namespace ADJ.WebApp.Controllers
       defaultModel.PODetails.Items = new List<OrderDetailDTO>();
       SetDropDownList();
       ViewBag.ItemId = -2;
-      ViewBag.PageSize = pageSize;
 
       return View(defaultModel);
     }
@@ -68,7 +67,6 @@ namespace ADJ.WebApp.Controllers
       SetDropDownList();
       string viewName = addModel.Method.Substring(0, addModel.Method.IndexOf(" "));
       ViewBag.Method = addModel.Method;
-      ViewBag.PageSize = pageSize;
 
 
       if ((addModel.PODetails == null) || (addModel.PODetails.Items == null))
@@ -102,7 +100,9 @@ namespace ADJ.WebApp.Controllers
         }
 
         //return RedirectToAction("Index", new { id = addModel.PONumber, method = viewName });
-        return RedirectToAction("Display", "Order");
+        //return RedirectToAction("Display", "Order");
+        ViewBag.Modal_id = addModel.PONumber;
+        ViewBag.Modal_method = viewName;
       }
 
       return View(viewName, addModel);
@@ -118,7 +118,6 @@ namespace ADJ.WebApp.Controllers
 
       SetDropDownList();
       ViewBag.CurrentPage = currentPage ?? 1;
-      ViewBag.PageSize = pageSize;
       if (newItem.Count > 0)
       {
         addModel.SingleOrderDetail = ConvertToDto(newItem)[0];
@@ -130,12 +129,6 @@ namespace ADJ.WebApp.Controllers
 
       switch (method)
       {
-        case "Previous":
-          ViewBag.CurrentPage--;
-          break;
-        case "Next":
-          ViewBag.CurrentPage++;
-          break;
         case "Save":
           if (addModel.PODetails == null) { addModel.PODetails = new PagedListResult<OrderDetailDTO>(); }
           if (addModel.PODetails.Items == null) { addModel.PODetails.Items = new List<OrderDetailDTO>(); }
@@ -216,7 +209,6 @@ namespace ADJ.WebApp.Controllers
 
       SetDropDownList();
       ViewBag.ItemId = -2;
-      ViewBag.PageSize = pageSize;
 
       return View(editModel);
     }
@@ -243,7 +235,6 @@ namespace ADJ.WebApp.Controllers
 
       SetDropDownList();
       ViewBag.ItemId = -2;
-      ViewBag.PageSize = pageSize;
 
       return View(viewModel);
     }
@@ -279,7 +270,6 @@ namespace ADJ.WebApp.Controllers
 
       copyModel.PONumber = "";
       ViewBag.ItemId = -2;
-      ViewBag.PageSize = pageSize;
 
       return View(copyModel);
     }
@@ -342,6 +332,7 @@ namespace ADJ.WebApp.Controllers
     //Set DropDownList to select on View
     private void SetDropDownList()
     {
+      ViewBag.PageSize = pageSize;
       ViewBag.Seasons = SeasonList();
       ViewBag.Ports = new List<string> { "Cẩm Phả", "Hòn Gai", "Hải Phòng", "Nghi Sơn", "Cửa Lò" };
       ViewBag.Modes = new List<string> { "Road", "Sea", "Air" };
