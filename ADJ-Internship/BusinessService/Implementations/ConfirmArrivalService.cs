f65199ac9eb2d8d9036a58c973b2bd5536d1ef71
Tôn Thất Duy Khang <duykhang91@gmail.com>
2018-12-29 10:46:48 +0700

    add filter service (without sorting) to ConfirmArrival Service

--- ADJ-Internship/BusinessService/Implementations/ConfirmArrivalService.cs
+++ ADJ-Internship/BusinessService/Implementations/ConfirmArrivalService.cs
@@ -4,8 +4,10 @@ using ADJ.Common;
 using ADJ.DataModel.ShipmentTrack;
 using ADJ.Repository.Core;
 using AutoMapper;
+using LinqKit;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +16,12 @@ namespace ADJ.BusinessService.Implementations
 {
   public class ConfirmArrivalService : ServiceBase, IConfirmArrivalService
   {
-    private readonly IDataProvider<Container> _containerProvider;
+    private readonly IDataProvider<Container> _containerDataProvider;
 
     public ConfirmArrivalService(IUnitOfWork unitOfWork, IMapper mapper, ApplicationContext appContext,
       IDataProvider<Container> containerDataProvider) : base(unitOfWork, mapper, appContext)
     {
-      _containerProvider = containerDataProvider;
+      _containerDataProvider = containerDataProvider;
     }
 
     public async Task<List<Container>> ListContainerFilterAsync(int? page, DateTime? ETAFrom, DateTime? ETATo, string origin = null, string mode = null, 
@@ -31,11 +33,56 @@ namespace ADJ.BusinessService.Implementations
 
       if (origin != null)
       {
-        //Expression<Func<Container, bool>> filter = x => x. == origin;
-        //All = All.And(filter);
+        Expression<Func<Container, bool>> filter = x => x.Manifests.Where(p => p.Booking.Order.Origin == origin).Count() > 0;
+        All = All.And(filter);
       }
 
-      return null;
+      if (mode != null)
+      {
+        Expression<Func<Container, bool>> filter = x => x.Manifests.Where(p => p.Booking.Mode == mode).Count() > 0;
+        All = All.And(filter);
+      }
+
+      if (vendor != null)
+      {
+        Expression<Func<Container, bool>> filter = x => x.Manifests.Where(p => p.Booking.Order.Vendor == vendor).Count() > 0;
+        All = All.And(filter);
+      }
+
+      if (container != null)
+      {
+        Expression<Func<Container, bool>> filter = x => x.Name == container;
+        All = All.And(filter);
+      }
+
+      if ((ETAFrom != null) || (ETATo != null))
+      {
+        status = ContainerStatus.Despatch.ToString();
+
+        if (ETAFrom != null)
+        {
+          Expression<Func<Container, bool>> filter = x => x.Manifests.Where(p => p.Booking.ETA.CompareTo(ETAFrom) > 0).Count() > 0;
+          All = All.And(filter);
+        }
+
+        if (ETATo != null)
+        {
+          Expression<Func<Container, bool>> filter = x => x.Manifests.Where(p => p.Booking.ETA.CompareTo(ETATo) < 0).Count() > 0;
+          All = All.And(filter);
+        }
+      }
+
+      if (status != null)
+      {
+        Expression<Func<Container, bool>> filter = x => x.Status.ToString() == status;
+        All = All.And(filter);
+      }
+
+      PagedListResult<Container> result = await _containerDataProvider.ListAsync(All, "PortOfDelivery", true);
+
+      return result.Items;
     }
+
+
   }
 }
