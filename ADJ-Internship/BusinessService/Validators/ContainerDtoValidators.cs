8c4223c5c2164acbd67007ec5f12cf7b42b11add
tannguyen2602 <tan.nguyen@groovetechnology.com>
2019-01-03 16:16:22 +0700

    commit-2

--- ADJ-Internship/BusinessService/Validators/ContainerDtoValidators.cs
+++ ADJ-Internship/BusinessService/Validators/ContainerDtoValidators.cs
@@ -5,31 +5,31 @@ using System.Text;
 
 namespace ADJ.BusinessService.Validators
 {
-  public class ContainerDtoValidators
-  {
-    public class ShipQuantityValidation : ValidationAttribute
-    {
-      private readonly string _otherProperty;
+	public class ContainerDtoValidators
+	{
+		public class QuantityValidation : ValidationAttribute
+		{
+			private readonly string _otherProperty;
 
-      public ShipQuantityValidation(string otherProperty)
-      {
-        _otherProperty = otherProperty;
-      }
+			public QuantityValidation(string otherProperty)
+			{
+				_otherProperty = otherProperty;
+			}
 
-      protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-      {
-        var otherValue = validationContext.ObjectType.GetProperty(_otherProperty).GetValue(validationContext.ObjectInstance, null);
-        if ((value != null) && (otherValue != null))
-        {
-          decimal shipQuantity = decimal.Parse(value.ToString());
-          decimal openQuantity = decimal.Parse(otherValue.ToString());
-          if (shipQuantity > openQuantity)
-          {
-            return new ValidationResult(ErrorMessage = "Ship Quantity is invalid, please try again");
-          }
-        }
-        return ValidationResult.Success;
-      }
-    }
-  }
+			protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+			{
+				var otherValue = validationContext.ObjectType.GetProperty(_otherProperty).GetValue(validationContext.ObjectInstance, null);
+				if ((value != null) && (otherValue != null))
+				{
+					decimal shipQuantity = decimal.Parse(value.ToString());
+					decimal openQuantity = decimal.Parse(otherValue.ToString());
+					if (shipQuantity > openQuantity)
+					{
+						return new ValidationResult(ErrorMessage = "Quantity is invalid, please try again");
+					}
+				}
+				return ValidationResult.Success;
+			}
+		}
+	}
 }
